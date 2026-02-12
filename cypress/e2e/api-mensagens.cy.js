9247a992a2fcd8836d322010a3508645e6aa83ec
Gabriel Marques <bielmarques922@gmail.com>
2026-02-12 16:48:16 -0300

    🧪 test(cypress): update API test to use Cypress environment variables for authorization

--- cypress/e2e/api-mensagens.cy.js
+++ cypress/e2e/api-mensagens.cy.js
@@ -1,11 +1,9 @@
 describe('Api Adopet', () => {
-  const authorization = `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMTY0M2NkNi03MTEyLTQxNWItOTVkMi0wNzkwNGIwZDFhMWMiLCJhZG9wdGVyTmFtZSI6IkFuYSBkZSBKZXN1cyIsImlhdCI6MTc3MDE0MjIzMSwiZXhwIjoxNzcwNDAxNDMxfQ.60oh6ZBbKLmBvCWdS3FAthEFAQAVUTm5KTx6A71a3G4`
-
   it('Mensagens da API', () => {
     cy.request({
       method: 'GET',
       url: 'https://adopet-api-i8qu.onrender.com/mensagem/11643cd6-7112-415b-95d2-07904b0d1a1c',
-      headers: { authorization }
+      headers: Cypress.env()
     }).then((res) => {
       expect(res.status).to.eq(200)
       expect(res.body).to.not.be.empty
